416709b4434c27c05243aa7d1d792ba612796d3f
burlin_gauna <burlin_gauna@yahoo.com>
2026-03-22 15:27:16 +0800

    created module

---
+++ ProjNexus/Source/CoreFramework/CoreFramework.Build.cs
@@ -0,0 +1,16 @@
+﻿using UnrealBuildTool;
+
+public class CoreFramework : ModuleRules
+{
+	public CoreFramework(ReadOnlyTargetRules Target) : base(Target)
+	{
+		PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
+
+		PublicDependencyModuleNames.AddRange(new string[]
+		{
+			"Core",
+			"CoreUObject",
+			"Engine"
+		});
+	}
+}
---
+++ ProjNexus/Source/CoreFramework/Private/CoreFrameworkModule.cpp
@@ -0,0 +1,11 @@
+﻿#include "CoreFrameworkModule.h"
+
+void FCoreFrameworkModule::StartupModule()
+{
+}
+
+void FCoreFrameworkModule::ShutdownModule()
+{
+}
+
+IMPLEMENT_MODULE(FCoreFrameworkModule, CoreFramework)

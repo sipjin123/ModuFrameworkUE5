fcc57485f184468d522e9134334f3b60cfce352e
burlin_gauna <burlin_gauna@yahoo.com>
2026-03-22 23:25:55 +0800

    Added subsystem class, registered blueprint to asset manager

--- ProjNexus/Source/InventoryFramework/Private/Subsystem/InventorySubsystem.cpp
+++ ProjNexus/Source/InventoryFramework/Private/Subsystem/InventorySubsystem.cpp
@@ -8,11 +8,26 @@ bool UInventorySubsystem::GetInventoryProvider(AActor* Actor, TScriptInterface<I
 		return false;
 	}
 
+	// 1. Check if actor itself implements it
 	if (Actor->Implements<UInventoryProvider>())
 	{
 		OutProvider.SetObject(Actor);
 		OutProvider.SetInterface(Cast<IInventoryProvider>(Actor));
 		return true;
+	}
+
+	// 2. Check components (THIS is what you’re missing)
+	TArray<UActorComponent*> Components;
+	Actor->GetComponents(Components);
+
+	for (UActorComponent* Comp : Components)
+	{
+		if (Comp && Comp->Implements<UInventoryProvider>())
+		{
+			OutProvider.SetObject(Comp);
+			OutProvider.SetInterface(Cast<IInventoryProvider>(Comp));
+			return true;
+		}
 	}
 
 	return false;
